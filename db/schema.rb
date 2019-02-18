6311009f11c4f189c1c03e369fd15f02de3811ea
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-18 11:28:32 -0600

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_190947) do
+ActiveRecord::Schema.define(version: 2019_02_18_162345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2019_02_13_190947) do
     t.datetime "updated_at", null: false
     t.string "name", limit: 64, null: false
     t.date "birthdate"
-    t.integer "type_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_02_13_190947) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
