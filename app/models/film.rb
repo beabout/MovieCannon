de13c28a319b69a263638f21128bb8ccd4264f10
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-19 13:14:26 -0600

    genre model, controller, etc added

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,7 +3,7 @@ class Film < ApplicationRecord
   has_one_attached :trailer_url
   has_one_attached :movie_poster
   has_one_attached :filmshot
-  has_many :genres
+  has_and_belongs_to_many :genres
 
   validates_presence_of :title
 
