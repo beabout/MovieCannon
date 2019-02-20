5816065562eab9521fac179f546b28dd595a010a
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-20 11:28:56 -0600

    removed unnecessary bootstrap files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
+
   def image_rating(film)
-    image_tag 'G.jpg' if film.mpaa_rating == "G"
-    image_tag 'PG.png' if film.mpaa_rating == "PG"
-    image_tag 'PG13.png' if film.mpaa_rating == "PG13"
-    image_tag 'R.png' if film.mpaa_rating == "R"
+    if film.mpaa_rating == "G" then image_tag 'G.jpg', {height: 24}
+    elsif film.mpaa_rating == "PG" then image_tag 'PG.png', {height: 24}
+    elsif film.mpaa_rating == "PG13" then image_tag 'PG13.png', {height: 24}
+    elsif film.mpaa_rating == "R" then image_tag 'R.png', {height: 24}
+    end
   end
 end
