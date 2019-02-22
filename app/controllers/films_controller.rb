01d14779c30a2c732db3cfb657c05546a264616b
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-22 14:55:47 -0600

    css adjustments and film#show formatting

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -10,6 +10,8 @@ class FilmsController < ApplicationController
   # Display contents (info, Persons, classifications) for a specific film
   def show
     @film = Film.find(params[:id])
+    # @people = Film.film_people
+    # @film_classif = Film.film_classifications
     # @reviews =
   end
 
