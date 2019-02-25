f9b8674daa2c12bb2da634359484ea0695f657f7
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-25 13:24:55 -0600

    review new form fixes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,10 +13,22 @@ class ReviewsController < ApplicationController
 
   def new
     @film = Film.find(params[:film_id])
+    @review = Review.new(user: current_user, film: @film)
   end
 
-  def comment_params
-    params.require(:review).permit(:title, :id)
+  def create
+    # @review = Review.create(params[:review].merge({ user: current_user, film: @film }))
+    @review = Review.create(review_params)
+    # @review = Review.create(user_id: params[:user_id], film_id: params[:film_id], description: params[:x])
+    # @review = Review.create(params[:review])
+  end
+
+  def delete
+
+  end
+
+  def review_params
+    params.require(:review).permit(:description,:user_id,:film_id)
   end
 
 end
